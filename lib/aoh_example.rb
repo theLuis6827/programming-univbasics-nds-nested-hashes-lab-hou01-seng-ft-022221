711922d6894d3b3582e5e3a047acf1c885f8093c
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-13 00:46:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,7 +24,7 @@ def literal_aoh
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
-    BRUCE_G
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
