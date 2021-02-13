530aa9158962033a7a2e3f0eb24ec9be2086885d
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-13 00:46:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,7 @@ def literal_aoh
   directors = [
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    PAT_M,
+    { name:  "Pat Monteseian", occupation: "Staff" },
     KATE_G,
     BRUCE_G
     ]
